56c662ae94d89bbbd43d08b23a83c5f9678f3923
znwabudike <zachary.nwabudike@gmail.com>
2014-10-22 13:23:13 -0700

    getting used to IntelliJ

--- src/com/github/znwabudike/androidpng2bmp/Converter.java
+++ src/com/github/znwabudike/androidpng2bmp/Converter.java
@@ -2,6 +2,9 @@ package com.github.znwabudike.androidpng2bmp;
 
 /**
  * Created by znwabudike on 10/22/14.
+ *
+ * This class houses the methods used to convert PNG to BMP
  */
 public class Converter {
+
 }
